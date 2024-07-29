5d379b09fd793583cf26c3f91ce5403df1666017
suuuuu-mer <soya010@hanmail.net>
2024-07-29 10:56:57 +0900

    Title: 두 사람, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/두 사람/two-person.py
+++ 240729/두 사람/two-person.py
@@ -9,7 +9,7 @@ age_b = int(d[0])
 sex_a = c[1]
 sex_b = d[1]
 
-if sex_a == "M" or sex_b == "M":
+if (sex_a == "M" and age_a >= 19) or (sex_b == "M" and age_b >= 19):
     print(1)
 else:
     print(0)
