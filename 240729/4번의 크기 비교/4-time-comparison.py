a3279a70417c1d5d90fbb794f56b6d623836c3e6
suuuuu-mer <soya010@hanmail.net>
2024-07-29 10:19:48 +0900

    Title: 4번의 크기 비교, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240729/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,14 @@
+a = int(input())
+
+first_list = input()
+sep = first_list.split()
+
+b = int(sep[0])
+c = int(sep[1])
+d = int(sep[2])
+e = int(sep[3])
+
+print(int(a > b))
+print(int(a > c))
+print(int(a > d))
+print(int(a > e))
