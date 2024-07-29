4b7d31abfc51f381f7d9941e2827e2d32ea372a6
suuuuu-mer <soya010@hanmail.net>
2024-07-29 10:03:11 +0900

    Title: 특정 조건 두 정수 비교, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 240729/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -7,9 +7,9 @@ second_num = int(b[1])
 if first_num < second_num:
     print(1, end = " ")
 else:
-    print(0)
+    print(0, end = " ")
 
 if first_num == second_num:
-    print(1, end = " ")
+    print(1)
 else:
     print(0)
