c3583c36aa4dcb1af776789cb77fd0f80e34b62f
suuuuu-mer <soya010@hanmail.net>
2024-07-29 10:16:42 +0900

    Title: 4가지 관계연산자, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240729/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,10 @@
+a = input()
+b = a.split()
+
+first_num = int(b[0])
+second_num = int(b[1])
+
+print(int(first_num >= second_num))
+print(int(first_num > second_num))
+print(int(first_num <= second_num))
+print(int(first_num < second_num))
