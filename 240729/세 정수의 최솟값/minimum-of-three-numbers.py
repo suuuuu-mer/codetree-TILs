e761381b22fc9c5a19cd9e70959b3f7397ec43f9
suuuuu-mer <soya010@hanmail.net>
2024-07-29 10:37:14 +0900

    Title: 세 정수의 최솟값, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240729/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,4 +1,13 @@
 a = input()
 b = a.split()
 
-print(min(b))
+first_num = int(b[0])
+second_num = int(b[1])
+third_num = int(b[2])
+
+if first_num <= second_num and first_num <= third_num:
+    print(first_num)
+elif second_num <= first_num and second_num <= third_num:
+    print(second_num)
+else:
+    print(third_num)
