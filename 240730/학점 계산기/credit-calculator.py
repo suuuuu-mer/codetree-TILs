6e882f8c3c80af4da351fe5dcbe045cba4eed00c
suuuuu-mer <soya010@hanmail.net>
2024-07-30 19:11:39 +0900

    Title: 학점 계산기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240730/학점 계산기/credit-calculator.py
@@ -0,0 +1,19 @@
+n = int(input())
+sub = input()
+list_sub = sub.split()
+
+sum_val = 0
+
+for i in range(n):
+    sum_val += float(list_sub[i])
+
+avr = sum_val / 4
+
+print(f"{avr:.1f}")
+
+if avr >= 4.0:
+    print("Perfect")
+elif avr >= 3.0:
+    print("Good")
+else:
+    print("Poor")
