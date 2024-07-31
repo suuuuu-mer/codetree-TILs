bd0ae3926af3d595db4459b33323796908d0bbdc
suuuuu-mer <soya010@hanmail.net>
2024-07-31 12:11:52 +0900

    Title: 점수대 파악하기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,14 @@
+num = list(map(int, input().split()))
+
+count = [0] * 11
+
+for i in range(len(num)):
+    if (num[i] // 10) < 0:
+        continue
+    else:
+        count[num[i] // 10] += 1
+
+count_rev = count[::-1]
+
+for j in range(10):
+    print(f"{100 - (j * 10)} - {count_rev[j]}")
