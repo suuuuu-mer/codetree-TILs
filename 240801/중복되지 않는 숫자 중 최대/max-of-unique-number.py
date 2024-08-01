006b04094f4757ff63156c8af625f5239d007b04
suuuuu-mer <soya010@hanmail.net>
2024-08-01 09:26:24 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240801/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240801/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -4,9 +4,9 @@ nums = list(map(int, input().split()))
 max_val = 0
 
 for i in nums:
-    if i > max_val:
-        max_val = i
-    elif i == max_val:
+    if i == max_val:
         max_val = -1
+    elif i > max_val:
+        max_val = i
 
 print(max_val)
